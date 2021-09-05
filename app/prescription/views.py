2b34d513e447a559cda9b1402cb70b6a31f0b135
glen-s-abraham <glenprojects666@gmail.com>
2021-09-05 10:06:03 +0530

    refactor(prescreption):custom creation and permissions added

--- app/prescription/views.py
+++ app/prescription/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from core.models import Prescription
 from prescription.serializers import PrescriptionSerializer
 
@@ -6,11 +8,10 @@ from prescription.serializers import PrescriptionSerializer
 class PrescriptionViewSet(viewsets.ModelViewSet):
     """View set from Prescriptions"""
     serializer_class = PrescriptionSerializer
-    queryset = Prescription.objects.all()
+    queryset = Prescription.objects.filter()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-
-"""
-TODO
-    1.add authentication
-    2.custom creation to add logged in user before submissions
-"""
+    def perform_create(self, serializer):
+        """create a new prescription"""
+        serializer.save(doctor=self.request.user)
